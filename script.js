c35e5b56ceb0956c04226c12872fc1c58061c91d
GillesP-dev <117928509+GillesP-dev@users.noreply.github.com>
2023-01-21 20:41:37 +0100

    amelioration fenetre image

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 
 let titre;
 let imageFilm;
+let totalTitres;
+let ma_recherch;
+let images;
 let tabtitre = []; 
-let modale = document.querySelector(".modal");
+let modale = document.querySelector("#modalFilm");
 let rechTitre = document.querySelector("#champRech");
 let fond = document.querySelector(".fondFilm");
 let fondFilm = document.querySelector("#fond");
-let imagesFilm = document.querySelector(".imageTitre");
+let titremsg = document.querySelector("h3");
+let imagesbox = document.querySelector("#surcouche #msgbox #imageBox");
+let boutonMsgBox = document.querySelector("#surcouche #msgbox #case");
+
+let surcouche1 = document.querySelector("#surcouche");
+
 let titreModale = document.querySelector(".modal-title");
-let ma_recherch = sessionStorage.getItem('search');
-console.log(titreModale);
+
+function selection(){
+
+    //let pointeur = event.target.dataset.key;
+       console.log("ca marche");
+       surcouche1.style.display = "block";
+  
+   }
 
 //-------------evenement recherche input enter---------------------------------------------
 rechTitre.addEventListener("keyup",(event) => {
@@ -18,9 +32,13 @@ rechTitre.addEventListener("keyup",(event) => {
         console.log(titre);
         rechTitre.value = "";
         sessionStorage.setItem('search',titre);
-        window.location.href = "/films.html";     
-    }})
+        
+        window.location.href = "/films.html"; 
+   
 
+}})
+ma_recherch = sessionStorage.getItem('search');
+recherche();
 //-------------evenement recherche input click---------------------------------------------  
 function rechTitres() {
     
@@ -28,17 +46,19 @@ function rechTitres() {
        console.log(titre);
        rechTitre.value = "";
        sessionStorage.setItem('search',titre);
-       window.location.href = "/films.html";     
+       window.location.href = "/films.html"; 
+          
    }
 
 
-    recherche()
+    
 //------------------------------fonction recherche-----------------------------------    
 async function recherche(){
+    
  await fetch ("http://www.omdbapi.com/?s="+ma_recherch+"&apikey=e19963b")
     .then(Response => Response.json())
     .then(data => {
-        let totalTitres = [...data.Search];
+         totalTitres = [...data.Search];
         console.log(totalTitres);
         
         for (let i=0; i<totalTitres.length;i++){
@@ -47,22 +67,34 @@ async function recherche(){
             tabtitre.push(totalTitres[i].Title) ;  
             imageFilm = document.createElement('img');
             imageFilm.setAttribute("src",totalTitres[i].Poster);
+            imageFilm.setAttribute("type","button");
             imageFilm.setAttribute("class","imageTitre class"+i);
-            imageFilm.setAttribute("data-bs-target","#modalFilm");
-            imageFilm.setAttribute("data-bs-toggle","modal");
-            fondFilm.append(imageFilm);  
+            imageFilm.setAttribute("id","imageTitre"+i);
+            imageFilm.setAttribute("data-key",i);
+            imageFilm.setAttribute("onclick","selection");
+                       fondFilm.append(imageFilm); 
+           
              
-            }
-            titreModale.textContent = totalTitres[i].Title; 
+            }  
         }
-        console.log(tabtitre);
+        if (fondFilm.hasChildNodes()) {
+            images = fondFilm.querySelectorAll("img");
+        }
+        console.log(images); 
     })
-    .catch(error =>console.error(error))
+   
+    .catch(error =>console.error(error));
+    images.forEach((image, index) => {
+        image.addEventListener("click", () => {
+            let titre = tabtitre[index];
+            surcouche1.style.display = "block";
+            titremsg.textContent = titre;
+            imagesbox.setAttribute("src",totalTitres[index].Poster);
+        });
+    }); 
 }
 
-// imagesFilm.addEventListener('click', (e)=>{  
-//      console.log("ça marche");
-//     console.log(titreModale) ;
-//     // modale.style.display = "block";
-//     titreModale.textContent = "";
-// }); 
+
+
+console.log(images); 
+boutonMsgBox.addEventListener('click',()=> surcouche1.style.display = "none")
